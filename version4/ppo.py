e00384908ee5801ca2d34a98af2de52cc6ca582c
danielpkhalil <117323103+danielpkhalil@users.noreply.github.com>
2025-03-07 02:31:58 -0500

    Update ppo.py

--- version4/ppo.py
+++ version4/ppo.py
@@ -344,7 +344,7 @@ if __name__ == "__main__":
         "LR": 2.5e-4,
         "NUM_ENVS": 4,
         "NUM_STEPS": 128,
-        "TOTAL_TIMESTEPS": 5e3,
+        "TOTAL_TIMESTEPS": 5e5,
         "UPDATE_EPOCHS": 4,
         "NUM_MINIBATCHES": 4,
 
