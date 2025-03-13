3aae808e7c5b2b419d434c565ec40d135ee600d1
danielpkhalil <danielpkhalil@gmail.com>
2025-03-13 03:57:44 -0700

    parallel seed with vmap

--- version8/ppo.py
+++ version8/ppo.py
@@ -370,7 +370,7 @@ if __name__ == "__main__":
         "LR": 2.5e-4,
         "NUM_ENVS": 4,
         "NUM_STEPS": 128,
-        "TOTAL_TIMESTEPS": 1e6,
+        "TOTAL_TIMESTEPS": 1e5,
         "UPDATE_EPOCHS": 4,
         "NUM_MINIBATCHES": 4,
 
