b3067b98c152369e16ee0b9f2eab210ec1c7f804
danielpkhalil <danielpkhalil@gmail.com>
2025-03-18 21:42:10 -0700

    minigrid cnn original batch size

--- version9/ppo.py
+++ version9/ppo.py
@@ -404,7 +404,7 @@ if __name__ == "__main__":
         "MAX_GRAD_NORM": 0.5,
         "ACTIVATION": "relu",
         "ENV_NAME": "TabularMDP",  # Example MiniGrid name
-        "ENV_FILE": "/nas/ucb/cassidy/rl-theory/data/mdps/MiniGrid-MultiRoom-N2-S4-v0/consolidated.npz",  # Not used unless you do "TabularMDP"
+        "ENV_FILE": "/nas/ucb/cassidy/rl-theory/data/mdps/MiniGrid-Empty-5x5-v0/consolidated.npz",  # Not used unless you do "TabularMDP"
         "REWARD_SCALE": 1.0,
         "EVAL_FREQUENCY": 1000,
         "TRAIN_MEDIAN_WINDOW": 20,
