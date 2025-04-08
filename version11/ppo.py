71f410bb25e8dc80d88adf71e018f6169f5df0ee
danielpkhalil <danielpkhalil@gmail.com>
2025-04-08 11:44:26 -0700

    sokoban 8 test

--- version11/ppo.py
+++ version11/ppo.py
@@ -403,7 +403,7 @@ if __name__ == "__main__":
     plt.close()
 
     # 5) Multi-seed example
-    num_seeds = 4
+    num_seeds = 10
     rng_seeds = jax.random.split(jax.random.PRNGKey(999), num_seeds)
     batched_train = jax.jit(jax.vmap(train_fn))
     t0 = time.time()
